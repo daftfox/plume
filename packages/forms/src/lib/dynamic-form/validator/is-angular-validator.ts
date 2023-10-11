c0a53c98790a0da57fced43c096319809b548072
Timothy Claassens <tim.claassens@gmail.com>
2023-10-11 16:01:39 +1300

    CI test

--- packages/forms/src/lib/dynamic-form/validator/is-angular-validator.ts
+++ packages/forms/src/lib/dynamic-form/validator/is-angular-validator.ts
@@ -1,6 +1,10 @@
 import { ValidatorFn, Validators } from '@angular/forms';
 import { PlumeValidatorFn } from './plume-validator-fn.interface';
 
+/**
+ * Checks whether the given validator reference is also found in Angular's Validator class
+ * @param validator
+ */
 export const isAngularValidator = (
   validator: ValidatorFn | PlumeValidatorFn,
 ): validator is ValidatorFn => {
